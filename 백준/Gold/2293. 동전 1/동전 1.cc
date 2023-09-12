716174273ea82d961400020d30007d6697eda0d5
kwaksh2319 <33820977+kwaksh2319@users.noreply.github.com>
2023-09-12 20:57:34 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,160 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<unordered_map>
+#include<map>
+#include<stack>
+using namespace std;
+
+struct Point {
+	int s, e, t, cnt, power;
+	char c;
+};
+
+void Prints(vector<vector<int>>maps, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << maps[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Prints(vector<vector<bool>>maps, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << maps[i][j] << " ";
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Prints(vector<vector<char>>maps, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << maps[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Prints(vector<vector<vector<char>>>maps, int n, int m, int k) {
+	for (int t = 0; t < k; t++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				cout << maps[t][i][j];
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Prints(vector<vector<vector<bool>>>maps, int n, int m, int k) {
+	for (int t = 0; t < k; t++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				cout << maps[t][i][j];
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Prints(vector<int>line, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << line[i] << ",";
+	}
+	cout << endl;
+}
+void Prints(string str, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << str[i] << ",";
+	}
+	cout << endl;
+}
+
+bool checkmaps(vector<vector<int>>maps, vector<vector<int>>anw, int n, int m) {
+	bool bCheck = false;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (maps[i][j] != anw[i][j]) {
+				bCheck = true;
+				i = n + 1;
+				j = m + 1;
+				continue;
+			}
+		}
+
+	}
+	return bCheck;
+
+}
+bool findstart(vector<vector<int>>maps, vector<vector<int>>anw, int n, int m, int) {
+	bool bCheck = false;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (maps[i][j] != anw[i][j]) {
+				bCheck = true;
+				i = n + 1;
+				j = m + 1;
+				continue;
+			}
+		}
+
+	}
+	return bCheck;
+
+}
+
+struct Data {
+	long long s;
+	long long t;
+
+};
+struct GDatas {
+	int change;
+	int index;
+
+	string times;
+
+};
+bool cmp(const Data& a, const Data& b) {
+	if (a.s == b.s) {
+		return a.t < b.t;
+	}
+
+	return a.s < b.s;
+}
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	int n,k;
+	vector<int>coins;
+	cin >> n >> k;
+	for (int i = 0; i < n; i++) {
+		int tmp;
+		cin >> tmp;
+		coins.push_back(tmp);
+	}
+	vector<int>dp;
+	for (int i = 0; i <= k; i++) {
+		dp.push_back(0);
+	}
+	dp[0] = 1;
+	for (int i = 0; i < coins.size(); i++) {
+		for (int j = coins[i]; j <= k; j++) {
+			dp[j] = dp[j] + dp[j - coins[i]];
+		}
+	}
+	cout << dp[k];
+	return 0;
+}
