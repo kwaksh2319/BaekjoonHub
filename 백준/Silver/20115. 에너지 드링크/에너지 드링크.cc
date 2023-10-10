b5bef03973bd7eded5693578e5cfe62331d4a541
kwaksh2319 <33820977+kwaksh2319@users.noreply.github.com>
2023-10-10 22:14:45 +0900

    [Silver III] Title: 에너지 드링크, Time: 84 ms, Memory: 3572 KB -BaekjoonHub

---
+++ 백준/Silver/20115. 에너지 드링크/에너지 드링크.cc
@@ -0,0 +1,105 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include<string>
+#include<algorithm>
+#include<cmath>
+#include<unordered_map>
+#include<map>
+#include<stack>
+using namespace std;
+
+struct Point {
+	int i, j, t, cnt;
+	char c;
+};
+
+void Prints(vector<vector<int>>maps, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << maps[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Prints(vector<vector<bool>>maps, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << maps[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Prints(vector<vector<char>>maps, int n, int m) {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cout << maps[i][j];
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Prints(vector<vector<vector<char>>>maps, int n, int m, int k) {
+	for (int t = 0; t < k; t++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				cout << maps[t][i][j];
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+
+void Prints(vector<vector<vector<bool>>>maps, int n, int m, int k) {
+	for (int t = 0; t < k; t++) {
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				cout << maps[t][i][j];
+			}
+			cout << endl;
+		}
+		cout << endl;
+	}
+	cout << endl;
+}
+void Prints(string str, int n) {
+	for (int i = 0; i < n; i++) {
+		cout << str[i] << ",";
+	}
+	cout << endl;
+}
+
+struct Data {
+	int score;
+	int index;
+};
+
+struct Word {
+	std::string word;
+	int count;
+};
+
+int main() {
+	int n;
+	cin >> n;
+	vector<double>num;
+	for (int i = 0; i < n; i++) {
+		double tmp;
+		cin >> tmp;
+		num.push_back(tmp);
+
+	}
+	sort(num.begin(), num.end(),greater<>());
+	double sum = num[0];
+	for (int i = 1; i < n; i++) {
+		//cout << num[i] / 2 << ",";
+		sum += num[i]/2;
+	}
+	cout << sum;
+	return 0;
+}
